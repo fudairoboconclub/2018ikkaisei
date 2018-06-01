21990de4e1f9855509780b00106981aa97887f33
FudaiRobocon <fudairobocon@gmail.com>
2018-06-01 17:15:53 +0900

    明るさ連続変化　4秒周期　大澤

--- _0601_oosawa_analogled/_0601_oosawa_analogled.ino
+++ _0601_oosawa_analogled/_0601_oosawa_analogled.ino
@@ -1,5 +1,7 @@
+#define MAX 255
 int ledPin=11;
-int value=255;
+int value=MAX;
+int dir=1;
 void setup() {
   pinMode(ledPin,OUTPUT);
   // put your setup code here, to run once:
@@ -8,8 +10,20 @@ void setup() {
 
 void loop() {
   analogWrite(ledPin,value);
+  delay(400/51);
+
+  if(value<=0)
+    dir=0;
+  if(value>=MAX)
+    dir=1;
+
+  if(dir==0)
+   value+=1;
+  else
+   value-=1; 
+   
   
-  
+ 
   // put your main code here, to run repeatedly:
 
 }
