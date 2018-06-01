91d258a5e9388121eba9acb2daa0f074d06d8643
FudaiRobocon <fudairobocon@gmail.com>
2018-06-01 16:02:40 +0900

    dir insert

--- _0601oosawa/_0601oosawa.ino
+++ _0601oosawa/_0601oosawa.ino
@@ -1,5 +1,6 @@
  int led=13;
  int tm=250;
+ int dir=1;
  void setup() {
   pinMode(led,OUTPUT);
   // put your setup code here, to run once:
@@ -7,11 +8,17 @@
 }
 
 void loop() {
+  if(tm<0){
+    tm=250;
+  }
   digitalWrite(led,HIGH);
-  delay(tm);
+  delay(tm); 
   digitalWrite(led,LOW);
   delay(tm);
-  tm-=50;
+  if(dir==0)
+   tm+=50;
+  else
+   tm-=50; 
   // put your main code here, to run repeatedly:
 
 }
