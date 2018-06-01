24ed4d7890ae0acc7bc1adcb3c131fcb208d9ca9
FudaiRobocon <fudairobocon@gmail.com>
2018-06-01 16:26:34 +0900

    段々増えて　段々減る

--- _0601oosawa/_0601oosawa.ino
+++ _0601oosawa/_0601oosawa.ino
@@ -1,6 +1,7 @@
  int led=13;
  int tm=250;
  int dir=1;
+ #define MAX 250
  void setup() {
   pinMode(led,OUTPUT);
   // put your setup code here, to run once:
@@ -8,17 +9,25 @@
 }
 
 void loop() {
-  if(tm<0){
-    tm=250;
-  }
+ 
+  
   digitalWrite(led,HIGH);
   delay(tm); 
   digitalWrite(led,LOW);
   delay(tm);
+ 
+  if(tm<=0)
+    dir=0;
+  if(tm>MAX)
+    dir=1;
+
   if(dir==0)
    tm+=50;
   else
    tm-=50; 
+ 
+ 
+  
   // put your main code here, to run repeatedly:
 
 }
