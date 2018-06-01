04d5337709f766467384aa2d95b23145ec9eda48
FudaiRobocon <fudairobocon@gmail.com>
2018-06-01 15:22:47 +0900

    led blink oosawa

---
+++ _0601oosawa/_0601oosawa.ino
@@ -0,0 +1,15 @@
+ int led=13;
+ void setup() {
+  pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(500);
+  digitalWrite(led,LOW);
+  delay(500);
+  // put your main code here, to run repeatedly:
+
+}
