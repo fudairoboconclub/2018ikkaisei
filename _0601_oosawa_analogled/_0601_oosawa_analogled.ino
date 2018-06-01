c92f6c59ca638da04ff3a6c9a8c082348bdbce3a
FudaiRobocon <fudairobocon@gmail.com>
2018-06-01 16:50:48 +0900

    アナログライト

---
+++ _0601_oosawa_analogled/_0601_oosawa_analogled.ino
@@ -0,0 +1,15 @@
+int ledPin=11;
+int value=255;
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  analogWrite(ledPin,value);
+  
+  
+  // put your main code here, to run repeatedly:
+
+}
