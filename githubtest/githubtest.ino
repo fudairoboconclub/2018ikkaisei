b38f1ac97a287e255330ca01c6edae22587866de
ClubPC <fudairobocon@gmail.com>
2018-06-05 14:26:58 +0900

    点滅間隔を変数に

--- githubtest/githubtest.ino
+++ githubtest/githubtest.ino
@@ -1,3 +1,5 @@
+int x=200;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(3,HIGH);
@@ -6,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(3,HIGH);
-  delay(50);
+  delay(x);
   digitalWrite(3,LOW);
-  delay(50);  
+  delay(x);  
 }
