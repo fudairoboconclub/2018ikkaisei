ee67e00e2486fb20bda190a7167cab5c39144fc8
ClubPC <fudairobocon@gmail.com>
2018-06-06 11:23:41 +0900

    井上　LED点灯増幅

--- _0605inoue/_0605inoue.ino
+++ _0605inoue/_0605inoue.ino
@@ -1,3 +1,5 @@
+int tm=100;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(10,OUTPUT);
@@ -7,5 +9,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(10,HIGH);
-
+  delay(tm);
+  digitalWrite(10,LOW);
+  delay(tm);
+  tm+=50;
+  if(tm>500){
+    tm=100;
+    }
 }
