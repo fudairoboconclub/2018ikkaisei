b36021744c22128993f161c34489e2cbb901ee24
FudaiRobocon <fudairobocon@gmail.com>
2018-06-15 16:52:50 +0900

    可変抵抗の値が大きくなれば明るくなる３段階大澤

--- _0611kahenteikoumatomeoosawa/_0611kahenteikoumatomeoosawa.ino
+++ _0611kahenteikoumatomeoosawa/_0611kahenteikoumatomeoosawa.ino
@@ -1,10 +1,29 @@
-int analogpin=A1;
+int analogpin=A1;//アナログ入力はANALOG INのピンを設定
 int led=10;
 int R;
 int Rread(){
   R=analogRead(analogpin);
   return R;
   }
+int I;
+int Iread1(){
+  if((R>=0)&&(R<=341)){
+  analogWrite(led,85);
+  Serial.println("I1");
+  }
+}
+int Iread2(){
+  if((R>=341)&&(R<=682)){
+  analogWrite(led,170);
+  Serial.println("I2");
+  }
+}
+int Iread3(){
+  if((R>=682)&&(R<=1023)){
+  analogWrite(led,255);
+  Serial.println("I3");
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -16,8 +35,16 @@ void setup() {
 
 void loop() {
    Rread();
-  Serial.println(R);Serial.println("R :");
-  delay(1000);
+
+ Iread1();
+
+ Iread2();
+
+ Iread3();
+
+ delay(1000);
+
+  
   // put your main code here, to run repeatedly:
 
 }
